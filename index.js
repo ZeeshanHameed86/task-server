c0661ed073b4832cf9eec88cc7f7b79e84e7b4a5
Zeeshan Hameed <93074888+ZeeshanHameed86@users.noreply.github.com>
2023-06-01 16:04:24 +0500

    updated

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
 
 connection();
 
